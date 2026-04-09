c99120fda2524cd7680676d718f684fae4ab456e
Quoc - Pham Ngoc <quoc-pn@komit-consulting.com>
2026-04-09 12:54:54 +0700

    [MIG] crm_lead_currency: Migration to 19.0

--- crm_lead_currency/__manifest__.py
+++ crm_lead_currency/__manifest__.py
@@ -8,7 +8,7 @@
     "maintainers": [
         "luisg123v",
     ],
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA,Odoo Community Association (OCA),Vauxoo",
     "website": "https://github.com/OCA/crm",
